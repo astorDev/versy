553a6d91f2ea9f5019a2d9e2feb62e12b33d4520
Egor Tarasov <pavluchenk0@yandex.ru>
2024-03-11 18:10:27 +0400

    updated to read stuff

--- calver/full.sh
+++ calver/full.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+printf "Calculating suffix..."
 export SUFFIX=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/main/suffix.sh | sh)
-curl -sSL https://raw.githubusercontent.com/astorDev/versy/main/calver/.sh | sh
+echo "Suffix is '$SUFFIX'"
+echo "Calculating version..."
+export VERSION=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/main/calver/.sh | sh)
+echo "Version is '$VERSION'"
