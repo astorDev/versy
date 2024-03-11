958eb1db87395d1f03c71a0def660113d9643f4e
Egor Tarasov <pavluchenk0@yandex.ru>
2024-03-11 18:05:06 +0400

    chmoded

--- calver/full.sh
+++ calver/full.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 export SUFFIX=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/main/suffix.sh | sh)
-
 curl -sSL https://raw.githubusercontent.com/astorDev/versy/main/calver/.sh | sh
