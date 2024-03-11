1ca135be36423820e32d2affe36cd0025e79e44a
Egor Tarasov <pavluchenk0@yandex.ru>
2024-03-11 17:58:11 +0400

    calver/full.sh setup

---
+++ calver/full.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export SUFFIX=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/main/suffix.sh | sh)
+
+curl -sSL https://raw.githubusercontent.com/astorDev/versy/main/calver/.sh | sh
