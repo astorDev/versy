c8bbbd8b3b2c4d80f8b8b617e9acc9b823d12d65
Egor Tarasov <pavluchenk0@yandex.ru>
2024-03-11 18:21:55 +0400

    inlined suffix

--- calver/full.sh
+++ calver/full.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 echo "Starting calver script (BRANCH='$BRANCH', MAIN='$MAIN', RUN='$RUN')"
 printf "Calculating suffix..."
+
+if [ "$BRANCH" = "" ]; then
+    echo "Required variable BRANCH is empty. exiting..."
+    exit 1
+fi
+
+if [ "${MAIN}" = "" ]; then
+    MAIN="main"
+fi
+
+if [ "$BRANCH" = "$MAIN" ]; then
+    echo "Branch is main - no suffix"
+    echo "SUFFIX="
+else
+    echo "Branch '$BRANCH' is not main - calculating suffix..."
+    #NORMALIZED=${BRANCH//\//-}
+    #echo "SUFFIX=-$NORMALIZED"
+fi
+
 curl -sSL https://raw.githubusercontent.com/astorDev/versy/powerful-calver/suffix.sh | sh
 # export SUFFIX=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/powerful-calver/suffix.sh | sh)
 # echo "Suffix is '$SUFFIX'"
