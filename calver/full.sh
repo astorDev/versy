7b5e265e3c63818c229ab546e7e6f3c9e3e14eea
Egor Tarasov <pavluchenk0@yandex.ru>
2024-03-11 18:13:35 +0400

    logging inputs

--- calver/full.sh
+++ calver/full.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "Starting calver script (BRANCH='$BRANCH', MAIN='$MAIN', RUN='$RUN')"
 printf "Calculating suffix..."
 export SUFFIX=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/main/suffix.sh | sh)
 echo "Suffix is '$SUFFIX'"
