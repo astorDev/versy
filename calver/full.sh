665826e8d2d59621a7bdd65bf1b31beddb01868e
Egor Tarasov <pavluchenk0@yandex.ru>
2024-03-11 20:21:14 +0400

    fixed path (#11)

--- calver/full.sh
+++ calver/full.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "Starting calver script (BRANCH='$BRANCH', MAIN='$MAIN', RUN='$RUN')"
 echo ""
-SUFFIX_OUTPUT=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/mainsuffix.sh | sh)
+SUFFIX_OUTPUT=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/main/suffix.sh | sh)
 echo "$SUFFIX_OUTPUT"
 echo ""
 export SUFFIX=$(echo "$SUFFIX_OUTPUT" | grep 'SUFFIX is' | awk '{print $3}')
 echo "Extracted SUFFIX '$SUFFIX' from previous output"
 echo ""
-VERSION_OUTPUT=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/maincalver/.sh | sh)
+VERSION_OUTPUT=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/main/calver/.sh | sh)
 echo "$VERSION_OUTPUT"
