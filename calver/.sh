008dfe8d3f8f8c78088b7920c654c0aeb309715d
Egor Tarasov <pavluchenk0@yandex.ru>
2024-03-11 19:22:35 +0400

    complete

--- calver/.sh
+++ calver/.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+echo "Calculating version (RUN='$RUN', SUFFIX='$SUFFIX')"
 
 if [ "$RUN" = "" ]; then
     echo "Required variable RUN is empty. exiting..."
     exit 1
 fi
 
-echo $(date +'%Y.1%m.1%d').$RUN$SUFFIX
+VERSION=$(date +'%Y.1%m.1%d').$RUN$SUFFIX
+echo "VERSION is $VERSION"
