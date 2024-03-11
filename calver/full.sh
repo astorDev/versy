b52d148cb6267b8bf746bfead69742f625c197ae
Egor Tarasov <pavluchenk0@yandex.ru>
2024-03-11 19:16:26 +0400

    added breaks

--- calver/full.sh
+++ calver/full.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "Starting calver script (BRANCH='$BRANCH', MAIN='$MAIN', RUN='$RUN')"
+echo ""
 SUFFIX_OUTPUT=$(curl -sSL https://raw.githubusercontent.com/astorDev/versy/powerful-calver/suffix.sh | sh)
 echo "$SUFFIX_OUTPUT"
+echo ""
 echo "Extracting SUFFIX from output..."
 SUFFIX=$(echo "$SUFFIX_OUTPUT" | grep 'SUFFIX is' | awk '{print $3}')
 echo "SUFFIX is '$SUFFIX'"
