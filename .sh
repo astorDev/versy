ec7652e490fa83e1848fc345615bff878301073a
Egor Tarasov <pavluchenk0@yandex.ru>
2024-03-14 12:47:55 +0400

    updated functions order

--- .sh
+++ .sh
@@ -1,16 +1,5 @@
 #! /bin/bash
 
-source() {
-    if [ "$SOURCING_URL" = "" ]; then
-        echo "Sourcing ./$1.sh by relative path"
-        . ./$1.sh
-    else
-        echo "Sourcing $SOURCING_URL/$1.sh"
-        curl -sSL "$SOURCING_URL/$1.sh" > ./x.sh
-        . ./x.sh
-    fi
-}
-
 log() {
     echo >&2 $1
 }
@@ -22,6 +11,17 @@ throw() {
 
 ret() {
     echo $1
+}
+
+source() {
+    if [ "$SOURCING_URL" = "" ]; then
+        echo "Sourcing ./$1.sh by relative path"
+        . ./$1.sh
+    else
+        echo "Sourcing $SOURCING_URL/$1.sh"
+        curl -sSL "$SOURCING_URL/$1.sh" > ./x.sh
+        . ./x.sh
+    fi
 }
 
 source suffix/
