49c2f33d83deceeb4955312ac3dfcb367caad2fc
Egor Tarasov <pavluchenk0@yandex.ru>
2024-11-02 01:10:16 +0400

    flutter multiplatform image

--- flutter/flutter-image.sh
+++
@@ -1,2 +0,0 @@
-docker build . -t vosarat/flutter:2024.111.101.3
-docker push vosarat/flutter:2024.111.101.1
---
+++ flutter/image.sh
@@ -0,0 +1,2 @@
+docker buildx build --platform linux/amd64,linux/arm64 . -t vosarat/flutter:2024.111.101.7
+docker push vosarat/flutter:2024.111.101.7
