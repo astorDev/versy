8c5eb0403aaee8720ce32a263cf07bb03df3e05a
Egor Tarasov <pavluchenk0@yandex.ru>
2024-11-04 17:25:09 +0400

    loading images from tar

--- push_docker/.sh
+++ push_docker/.sh
@@ -30,9 +30,13 @@ if [ "$DOCKERFILE_PATH" = "" ]; then
         DOCKERFILE_PATH="PATH/Dockerfile"
     fi
 
-    BUILD_COMMAND="docker buildx build --platform linux/amd64,linux/arm64 $BUILD_CONTEXT_PATH --file $DOCKERFILE_PATH --tag $IMAGE:$VERSION"
+    BUILD_COMMAND="docker buildx build --platform linux/amd64,linux/arm64  -o type=docker,dest=built.tar . $BUILD_CONTEXT_PATH --file $DOCKERFILE_PATH --tag $IMAGE:$VERSION"
     log "Executing: $BUILD_COMMAND"
     $BUILD_COMMAND
+
+    LOAD_COMMAND="docker load -i built.tar"
+    log "Executing: $LOAD_COMMAND"
+    $LOAD_COMMAND
 
     if (echo "$VERSION" | grep -q "-"); then # () is important to preserve execution order.
         log "VERSION contains a hyphen. Not tagging as latest."
