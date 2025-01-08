92d54caf852475a3eeb94183f736c56a10e5bd73
Egor Tarasov <pavluchenk0@yandex.ru>
2025-01-08 13:37:52 +0400

    utilizing nice shell

--- .sh
+++ .sh
@@ -1,21 +1,6 @@
 #! /bin/bash
-LIGHT_CYAN='\033[1;36m'
-RED='\033[31m'
-NC='\033[0m'
-
-log() {
-    printf "${LIGHT_CYAN}${1}${NC}\n" >&2
-}
-
-throw() {
-    printf "${RED}${1}. Exiting...${NC}\n" >&2 
-    exit 1
-}
-
-ret() {
-    printf %s "${1}"
-    printf "\n" >&2
-}
+. /dev/stdin <<< "$(curl -sS https://raw.githubusercontent.com/astorDev/nice-shell/main/.sh)"
+log "Sourced nice-shell"
 
 sourcing() {
     if [ "$SOURCING_URL" = "" ]; then
