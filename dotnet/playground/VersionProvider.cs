82f032c12ab2774d10ff71bb8ba64e246ca358d6
Egor Tarasov <pavluchenk0@yandex.ru>
2025-03-15 14:12:01 +0300

    dotnet VersionProvider lib and playground

---
+++ dotnet/playground/VersionProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace Versy.V0;
+
+public class VersionProvider(IConfiguration configuration)
+{
+    public string Get()
+    {
+        return 
+            configuration["Version"] ??
+            Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? 
+            "Unknown";
+    }
+}
+
+public static class Registrator
+{
+    public static IServiceCollection AddVersionProvider(this IServiceCollection services)
+    {
+        services.AddSingleton<VersionProvider>();
+        return services;
+    }
+}
